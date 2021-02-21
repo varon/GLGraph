0a3223f8705b29f082c415700f66faa82ef7d2fe
varon <varon-github@outlook.com>
2021-02-21 10:57:01 +0200

    Nuke build

--- build/Build.cs
+++ build/Build.cs
@@ -32,7 +32,7 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    [GitVersion(NoFetch = true, Framework = "netcoreapp3.1")] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
